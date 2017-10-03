5039cfe2f94e6bd9fb663cd65f861d69bfd489e3
ya90net <ya90net@yahoo.co.jp>
2017-10-03 22:21:47 +0800

    add chapter V

--- common/js/script.js
+++ common/js/script.js
@@ -122,6 +122,173 @@ ioc.controller('Container', function($scope) {
 	};
 
 
+	// V
+	// 1
+	$scope.color2_1 = new Object();
+	$scope.color2_2 = new Object();
+
+	$scope.color2_1.color = "#238aec";
+	$scope.color2_2.color = "#3192ed";
+
+	$scope.color2_1.option = {
+	    default: $scope.color1.color,
+	    hasBackdrop : false,
+	};
+
+	$scope.color2_2.option = {
+	    default: $scope.color2.color,
+	    hasBackdrop : false,
+	};
+
+	// 2
+	$scope.color2_3 = new Object();
+	$scope.color2_4 = new Object();
+	$scope.color2_5 = new Object();
+
+	$scope.color2_3.color = "#232323";
+	$scope.color2_4.color = "#7f7f7f";
+	$scope.color2_5.color = "#cecece";
+
+	$scope.color2_3.option = {
+	    default: $scope.color2_3.color,
+	    hasBackdrop : false,
+	};
+	$scope.color2_4.option = {
+	    default: $scope.color2_4.color,
+	    hasBackdrop : false,
+	};
+	$scope.color2_5.option = {
+	    default: $scope.color2_5.color,
+	    hasBackdrop : false,
+	};
+
+	// 3
+	$scope.color2_6 = new Object();
+	$scope.color2_7 = new Object();
+	$scope.color2_8 = new Object();
+
+	$scope.color2_6.color = "#232323";
+	$scope.color2_7.color = "#7f7f7f";
+	$scope.color2_8.color = "#cecece";
+
+	$scope.color2_6.option = {
+	    default: $scope.color2_6.color,
+	    hasBackdrop : false,
+	};
+	$scope.color2_7.option = {
+	    default: $scope.color2_7.color,
+	    hasBackdrop : false,
+	};
+	$scope.color2_8.option = {
+	    default: $scope.color2_8.color,
+	    hasBackdrop : false,
+	};
+
+	// 4
+	$scope.color2_9 = new Object();
+	$scope.color2_10 = new Object();
+	$scope.color2_11 = new Object();
+	$scope.color2_12 = new Object();
+	$scope.color2_13 = new Object();
+	$scope.color2_14 = new Object();
+	$scope.color2_15 = new Object();
+	$scope.color2_16 = new Object();
+
+	$scope.color2_9.color = "#658dcb";
+	$scope.color2_10.color = "#060658";
+	$scope.color2_11.color = "#15088f";
+	$scope.color2_12.color = "#0740de";
+	$scope.color2_13.color = "#0026d7";
+	$scope.color2_14.color = "#007dd7";
+	$scope.color2_15.color = "#1e6c87";
+	$scope.color2_16.color = "#001c6b";
+
+	$scope.color2_9.option = {
+	    default: $scope.color2_6.color,
+	    hasBackdrop : false,
+	};
+	$scope.color2_10.option = {
+	    default: $scope.color2_7.color,
+	    hasBackdrop : false,
+	};
+	$scope.color2_11.option = {
+	    default: $scope.color2_8.color,
+	    hasBackdrop : false,
+	};
+	$scope.color2_12.option = {
+	    default: $scope.color2_8.color,
+	    hasBackdrop : false,
+	};
+	$scope.color2_13.option = {
+	    default: $scope.color2_6.color,
+	    hasBackdrop : false,
+	};
+	$scope.color2_14.option = {
+	    default: $scope.color2_7.color,
+	    hasBackdrop : false,
+	};
+	$scope.color2_15.option = {
+	    default: $scope.color2_8.color,
+	    hasBackdrop : false,
+	};
+	$scope.color2_16.option = {
+	    default: $scope.color2_8.color,
+	    hasBackdrop : false,
+	};
+
+
+	// 5
+	$scope.color2_17 = new Object();
+	$scope.color2_18 = new Object();
+	$scope.color2_19 = new Object();
+	$scope.color2_20 = new Object();
+	$scope.color2_21 = new Object();
+	$scope.color2_22 = new Object();
+	$scope.color2_23 = new Object();
+	$scope.color2_24 = new Object();
+
+	$scope.color2_17.color = "#9e0101";
+	$scope.color2_18.color = "#d11414";
+	$scope.color2_19.color = "#a00d0d";
+	$scope.color2_20.color = "#da5959";
+	$scope.color2_21.color = "#ab1a1a";
+	$scope.color2_22.color = "#df0d37";
+	$scope.color2_23.color = "#e10318";
+	$scope.color2_24.color = "#df3847";
+
+	$scope.color2_17.option = {
+	    default: $scope.color2_6.color,
+	    hasBackdrop : false,
+	};
+	$scope.color2_18.option = {
+	    default: $scope.color2_7.color,
+	    hasBackdrop : false,
+	};
+	$scope.color2_19.option = {
+	    default: $scope.color2_8.color,
+	    hasBackdrop : false,
+	};
+	$scope.color2_20.option = {
+	    default: $scope.color2_8.color,
+	    hasBackdrop : false,
+	};
+	$scope.color2_21.option = {
+	    default: $scope.color2_6.color,
+	    hasBackdrop : false,
+	};
+	$scope.color2_22.option = {
+	    default: $scope.color2_7.color,
+	    hasBackdrop : false,
+	};
+	$scope.color2_23.option = {
+	    default: $scope.color2_8.color,
+	    hasBackdrop : false,
+	};
+	$scope.color2_24.option = {
+	    default: $scope.color2_8.color,
+	    hasBackdrop : false,
+	};
+
 
 
 	// realtime color reflection
