b0a8d8c5f0647883ca1dfe0b5711d1b590719bf0
ya90net <contact@findyourmojo.xyz>
2017-12-21 22:55:53 +0800

    added chapterXX

--- app/scripts/controllers/chapterxx.js
+++ app/scripts/controllers/chapterxx.js
@@ -19,27 +19,9 @@ angular.module('interactionofColorApp')
 		color1 : ColorPickerService.makeColorPicker("#caf50a"),
 	};
 
-	$scope.range = function(n) {
-	    var arr = [];
-	    for (var i=0; i<n; ++i) arr.push(i);
-	    return arr;
+	$scope.plate2 = {
+		color1 : ColorPickerService.makeColorPicker("#651d16"),
 	};
 
-	$scope.getColor = function(){
-		var num = $scope.getRandomInt(5, 8);
-		return 'plate1.color' + num + '.color';
-	};
-
-	$scope.getRandomInt = function(min, max){
-		return Math.floor( Math.random() * (max + 1 - min) ) + min ;
-	}
-
-	$scope.count = 300;
-	$scope.$watch('count', function () {
-		for (var a = [], i = 0; i < $scope.count; i++) {
-	      a.push({ attr: $scope.getColor() });
-	    }
-	    $scope.Colors = a;
-	});
 
   });
