a5d58d9a21afe43bf2fe8818da348de1e7ff56b7
ya90net <contact@findyourmojo.xyz>
2018-01-01 16:31:31 +0800

    fixed chapterXVII

--- app/scripts/controllers/chapterxvii.js
+++ app/scripts/controllers/chapterxvii.js
@@ -40,37 +40,5 @@ angular.module('interactionofColorApp')
 		color8 : ColorPickerService.makeColorPicker("#224008"),
 	};
 
-	// plate3
-	$scope.plate3 = {
-		color1 : ColorPickerService.makeColorPicker("#ab905a"),
-		color2 : ColorPickerService.makeColorPicker("#908e8f"),
-
-		color3 : ColorPickerService.makeColorPicker("#145234"),
-		color4 : ColorPickerService.makeColorPicker("#3da779"),
-		color5 : ColorPickerService.makeColorPicker("#dbb765"),
-		color6 : ColorPickerService.makeColorPicker("#e93f15"),
-	};
-
-	// plate4
-	$scope.plate4 = {
-		color1 : ColorPickerService.makeColorPicker("#cdd9b6"),
-		color2 : ColorPickerService.makeColorPicker("#bc8bd5"),
-		color3 : ColorPickerService.makeColorPicker("#dd3636"),
-		color4 : ColorPickerService.makeColorPicker("#114f09"),
-	};
-
-	
-
-	// onLoad
-	angular.element(document).ready(function () {
-	    $(".plate4 .illustration > .color > .color").each(function(index, el) {
-	    	var min = 0 ;
-			var max = 20 ;
-			var rand = Math.floor( Math.random() * (max + 1 - min) ) + min ;
-	    	var left = rand - 10;
-	    	var rotate = Math.floor(rand / 4 - 2.5);
-			$(el).css({left: left + 'px', transform:'rotate(' + rotate + 'deg)'});
-		});
-	});
 
   });
