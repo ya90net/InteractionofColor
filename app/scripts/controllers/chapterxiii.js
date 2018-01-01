5a2afbaacdd319cbf4f1c7dc104708fde2a4cc3e
ya90net <contact@findyourmojo.xyz>
2018-01-01 15:51:49 +0800

    fixed chapterXIII

--- app/scripts/controllers/chapterxiii.js
+++ app/scripts/controllers/chapterxiii.js
@@ -13,7 +13,6 @@ angular.module('interactionofColorApp')
 	////
 	// make color pickers
 
-
 	// plate1
 	$scope.plate1 = {
 		color1 : ColorPickerService.makeColorPicker("#000000"),
@@ -24,27 +23,19 @@ angular.module('interactionofColorApp')
 	// plate2
 	$scope.plate2 = {
 		color1 : ColorPickerService.makeColorPicker("#111111"),
-		color2 : ColorPickerService.makeColorPicker("#972b2b"),
-		color3 : ColorPickerService.makeColorPicker("#d7568d"),
-		color4 : ColorPickerService.makeColorPicker("#a98f9e"),
+		color2 : ColorPickerService.makeColorPicker("#ffffff"),
+		color3 : ColorPickerService.makeColorPicker("#972b2b"),
+		color4 : ColorPickerService.makeColorPicker("#d7568d"),
+		color5 : ColorPickerService.makeColorPicker("#a98f9e"),
 	};
 
 	// plate3
 	$scope.plate3 = {
-		color1 : ColorPickerService.makeColorPicker("#ffffff"),
-		color2 : ColorPickerService.makeColorPicker("#972b2b"),
-		color3 : ColorPickerService.makeColorPicker("#d7568d"),
-		color4 : ColorPickerService.makeColorPicker("#a98f9e"),
-	};
-
-	// plate4
-	$scope.plate4 = {
 		color1 : ColorPickerService.makeColorPicker("#5a4242"),
 		color2 : ColorPickerService.makeColorPicker("#f2a0c9"),
 		color3 : ColorPickerService.makeColorPicker("#ffffff"),
 		color4 : ColorPickerService.makeColorPicker("#000000"),
 		color5 : ColorPickerService.makeColorPicker("#d01212"),
 	};
-
 
   });
